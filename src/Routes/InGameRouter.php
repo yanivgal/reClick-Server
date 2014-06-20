82001f36befc9ab787fa56766f73783140553265
Yaniv Gal <yanivgal123@gmail.com>
2014-06-20 21:42:57 +0300

    Add POST /games/:gameId/players/:username/sequence/ route for a player move

--- src/Routes/InGameRouter.php
+++ src/Routes/InGameRouter.php
@@ -6,6 +6,7 @@ use reClick\Controllers\Games\Game;
 use reClick\Controllers\Players\Player;
 use reClick\Framework\ResponseMessage;
 use reClick\GCM\GCM;
+use Slim\Slim;
 
 class InGameRouter extends BaseRouter {
 
@@ -15,42 +16,30 @@ class InGameRouter extends BaseRouter {
 
     protected function initializeRoutes() {
         $this->app->delete(
-            '/games/:gameId/players/:playerId/',
+            '/games/:gameId/players/:username/',
             [$this, 'deletePlayerFromGame']
+        );
+
+        $this->app->post(
+            '/games/:gameId/players/:username/sequence/',
+            [$this, 'playerMove']
         );
     }
 
     /**
-     * DELETE /games/:gameId/Players/:playerId
+     * DELETE /games/:gameId/Players/:username
      *
-     * @param $gameId
-     * @param $playerId
+     * @param int $gameId
+     * @param string $username
      */
-    public function deletePlayerFromGame($gameId, $playerId) {
+    public function deletePlayerFromGame($gameId, $username) {
         $game = new Game($gameId);
-        if (!$game->exists()) {
-            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
-                ->message('Game does not exist')
-                ->send();
-            exit;
-        }
+        $player = new Player($username);
 
-        $player = new Player($playerId);
-        if (!$player->exists()) {
-            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
-                ->message('Player does not exist')
-                ->send();
-            exit;
-        }
-        if (!$player->existsInGame($gameId)) {
-            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
-                ->message('Player does not exist in game')
-                ->send();
-            exit;
-        }
+        $this->checkExistence($game, $player);
 
         $previousPlayer = $game->previousPlayer();
-        $game->removePlayer($playerId);
+        $game->removePlayer($username);
         $players = $game->players();
 
         $gcm = new GCM();
@@ -73,5 +62,63 @@ class InGameRouter extends BaseRouter {
             ->message('Player removed from game')
             ->addData('game', $game->toArray())
             ->send();
+    }
+
+    /**
+     * POST /games/:gameId/players/:username/sequence/
+     *
+     * @param int $gameId
+     * @param string $username
+     */
+    public function playerMove($gameId, $username) {
+        $app = Slim::getInstance();
+
+        $game = new Game($gameId);
+        $player = new Player($username);
+
+        $this->checkExistence($game, $player);
+
+        $vars = parent::initJsonVars(
+            $app->request->getBody(),
+            ['sequence']
+        );
+
+        $game->playerMove($vars['sequence']);
+
+        $gcm = new GCM();
+        $gcm->message()
+            ->addData(
+                'message',
+                $player->nickname() . ' played his move, now it\' your turn '
+            )
+            ->addRegistrationId($game->currentPlayer()->gcmRegId());
+        $gcm->sendMessage();
+
+        (new ResponseMessage(ResponseMessage::STATUS_SUCCESS))
+            ->message('Player played his move')
+            ->send();
+    }
+
+    private function checkExistence(Game $game, Player $player) {
+        if (!$game->exists()) {
+            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
+                ->message('Game does not exist')
+                ->send();
+            exit;
+        }
+
+        if (!$player->exists()) {
+            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
+                ->message('Player does not exist')
+                ->send();
+            exit;
+        }
+
+        if (!$player->existsInGame($game->id())) {
+            (new ResponseMessage(ResponseMessage::STATUS_FAIL))
+                ->message('Player does not exist in game')
+                ->send();
+            exit;
+        }
     }
 } 
